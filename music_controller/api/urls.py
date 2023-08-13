c4205af13d4e996dabcc98b9ea113ca121c9bfa1
iamlydial <lydia.lavecchia@gmail.com>
2023-08-13 16:19:57 +0200

    added FE room join code, BE view and URL

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, get_user_id
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom,  get_user_id
 
 urlpatterns = [
     path("room/", RoomView.as_view()),
     path("create/", CreateRoomView.as_view()),
     path("room/<str:roomCode>", RoomView.as_view()),
     path("get-room/", GetRoom.as_view(),  name="get-room"),
-    path("get-user-id/", get_user_id, name="get_user_id")
+    path("get-user-id/", get_user_id, name="get_user_id"),
+    path("join", JoinRoom.as_view(), name="create")
 ]
--- music_controller/frontend/src/pages/JoinRoomPage/index.jsx
+++ music_controller/frontend/src/pages/JoinRoomPage/index.jsx
@@ -1,9 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button, Grid, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const JoinRoomPage = () => {
+  const [roomCode, setRoomCode] = useState("");
+  const [error, setError] = useState(false);
+  const [clickJoinRoom, setClickJoinRoom] = useState(null);
+
+  const handleTextFieldChange = (e) => {
+    setRoomCode(e.target.value);
+    setError(false);
+  };
+
+  const navigate = useNavigate();
+
+  const handleRoomButtonClicked = (e) => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        code: roomCode,
+      }),
+    };
+
+    fetch("http://127.0.0.1:8000/api/join", requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          return response.json(); // Parse response JSON if successful
+        } else {
+          throw new Error("Room not found"); // Throw an error if not successful
+        }
+      })
+      .then((data) => {
+        if (data.message === "Room Joined!") {
+          navigate("/room/" + roomCode); // Navigate on success
+        } else {
+          setError(true); // Set the error state to true
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(true); // Set the error state to true
+      });
+  };
+
   return (
     <>
-      <h1>JoinRoomPage</h1>
+      <Grid className="main-grid" container spacing={1}>
+        <Grid items xs={12} align="center">
+          <Typography variant="h4">Join a Room</Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <TextField
+            error="error"
+            label="Enter a Room Code"
+            placeholder="Ex: 012345"
+            value={roomCode}
+            helperText={error ? "Room not found" : ""}
+            variant="outlined"
+            onChange={handleTextFieldChange}
+          />
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button
+            variant="contained"
+            color="primary"
+            component={Link}
+            onClick={handleRoomButtonClicked}
+          >
+            Enter Room
+          </Button>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button variant="contained" color="secondary" to="/" component={Link}>
+            Back
+          </Button>
+        </Grid>
+      </Grid>
     </>
   );
 };
